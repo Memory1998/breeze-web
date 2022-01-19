2a03b3a03fdd6234009d467b7f7f60aecee5979f
breeze <breeze-cloud@foxmail.com>
2022-01-19 21:13:30 +0800

    格式化

--- src/components/UserLogin.vue
+++ src/components/UserLogin.vue
@@ -1,16 +1,32 @@
 <template>
-  <div class="userLogin">
-    <el-form :model="formLogin">
-      <el-form-item>
-        <el-input v-model="formLogin.username" placeholder="账号"></el-input>
+  <div>
+    <el-form
+      :rules="rules"
+      :model="userLogin"
+      ref="userLogin"
+      class="userLogin"
+    >
+      <h3>登录</h3>
+      <el-form-item prop="username">
+        <el-input
+          type="text"
+          auto-complete="false"
+          v-model="userLogin.username"
+          placeholder="账号"
+        ></el-input>
+      </el-form-item>
+      <el-form-item prop="password">
+        <el-input
+          type="password"
+          auto-complete="false"
+          v-model="userLogin.password"
+          placeholder="密码"
+        ></el-input>
       </el-form-item>
       <el-form-item>
-        <el-input v-model="formLogin.password" placeholder="密码"></el-input>
-      </el-form-item>
-      <el-form-item>
-        <el-button style="width: 100%" type="primary" @click="onSubmit">{{
-          loginBtn
-        }}</el-button>
+        <el-button style="width: 100%" type="primary" @click="onSubmit"
+          >{{ loginBtn }}
+        </el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -22,19 +38,56 @@ export default {
   data() {
     return {
       loginBtn: "登录",
-      formLogin: {
-        username: "",
-        password: "",
+      userLogin: {
+        username: "admin",
+        password: "123456",
+      },
+      rules: {
+        username: [
+          { required: true, message: "请输入登录账户", trigger: "blur" },
+        ],
+        password: [
+          { required: true, message: "请输入登录密码", trigger: "blur" },
+        ],
       },
     };
   },
   methods: {
     onSubmit() {
-      console.log("submit!");
+      this.$refs.userLogin.validate((valid) => {
+        if (valid) {
+          this.$message({
+            showClose: true,
+            message: "登录成功",
+            type: "success",
+          });
+        } else {
+          this.$message({
+            showClose: true,
+            message: "登录失败",
+            type: "error",
+          });
+          return false;
+        }
+      });
     },
   },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped></style>
+<style scoped>
+.userLogin {
+  border-radius: 15px;
+  background-clip: padding-box;
+  margin: 30vh auto;
+  padding: 2vh 2vw;
+  width: 18vw;
+  border: 1px #eaeaea solid;
+  box-shadow: 0 0 25px #9b9b9b;
+}
+
+.userLogin > h3 {
+  text-align: center;
+}
+</style>
