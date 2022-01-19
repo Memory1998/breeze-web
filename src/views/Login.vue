4bc45962f572386eec7b74bce73519fd1e04f503
breeze <breeze-cloud@foxmail.com>
2022-01-19 12:50:40 +0800

    initial project

---
+++ src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="loign">
+    <UserLogin/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import UserLogin from "@/components/UserLogin.vue";
+
+export default {
+  name: "Login",
+  components: {
+    UserLogin,
+  },
+};
+</script>
---
+++ src/views/user/User.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    User
+  </div>
+</template>
+
+<script>
+export default {
+  name: "User",
+  components: {
+  },
+};
+</script>
