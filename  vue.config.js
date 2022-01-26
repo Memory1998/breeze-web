6cb5c4630fb028fe99adb76aa6c5ee6db384d87a
breeze <breeze-cloud@foxmail.com>
2022-01-26 17:44:47 +0800

    配置

---  vue.config.js
+++  vue.config.js
@@ -1,24 +1,21 @@
-const baseURL = process.env.VUE_APP_BASE_API;
-debugger
+// const baseURL = process.env.VUE_APP_BASE_API;
 module.exports = {
+  //devServer.proxy适用于本地开发使用，
+  //生成环境请用第三方代理软件，如nginx。
   devServer: {
-    port: 8000,
-    open: true,
-    https: false,
-    overlay: {
-      warnings: false,
-      errors: true
-    },
-    proxy: { //配置跨域
+    port: 8080, //本机端口号
+    host: "localhost", //本机主机名
+    https: false, //协议
+    open: true, //启动服务器时自动打开浏览器访问
+    proxy: {
       '/api': {
-        target: baseURL, //这里后台的地址模拟的;应该填写你们真实的后台接口
-        changOrigin: true,
+        //目标服务器,代理访问到http://localhost:8888
+        target: "http://localhost:9999",
+        changOrigin: true, //开启代理
         pathRewrite: {
           '^/api': ''
         }
-      },
-    },
-    // before: require('./mock/mock-server.js')
+      }
+    }
   }
-};
-
+}
